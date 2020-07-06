814ec16f928f87d21bc82dd6986cfd293c75710d
Rolls47 <quentin.adadain@gmail.com>
2020-07-06 21:07:19 +0200

    Routeadmin for crud no security

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/portfolio")
+ * @Route("/admin/portfolio")
  */
 class PortfolioController extends AbstractController
 {
