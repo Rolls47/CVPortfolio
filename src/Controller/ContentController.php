b7226d4e9e233d36252db3cf499faf1eba58132e
Rolls47 <quentin.adadain@gmail.com>
2020-07-09 00:27:34 +0200

    first view skill and type

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Repository\FormationRepository;
 use App\Repository\JobRepository;
+use App\Repository\SkillRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -33,6 +34,18 @@ public function formation(FormationRepository $formationRepository): Response
 {
     return $this->render('content/formation.html.twig', [
         'formations' => $formationRepository->findAll(),
+    ]);
+}
+
+    /**
+     * @Route("/skill", name="skill")
+     * @param SkillRepository $skillRepository
+     * @return Response
+     */
+public function skill(SkillRepository $skillRepository)
+{
+    return $this->render('content/skill.html.twig', [
+        'skills' => $skillRepository->findAll(),
     ]);
 }
 }
